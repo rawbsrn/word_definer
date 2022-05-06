cfb98ad01a8b9bffcad9e18bb3d258d0c56bdddd
Rob Sameh <rob.sameh@gmail.com>
2022-05-06 13:43:15 -0700

    wrote out word.rb

---
+++ lib/word.rb
@@ -0,0 +1,41 @@
+class Word
+  attr_reader :id, :name
+  attr_accessor :name
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.name, self.id)
+  end
+
+  def ==(word_to_compare)
+    self.name() == word_to_compare.name()
+  end
+
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@words[id]
+  end
+
+  def update(name)
+    self.name = name
+    @@words[self.id] = Word.new(self.name, self.id)
+  end
+
+  def delete()
+    @@words.delete(self.id)
+  end
+end
