d6120a1137bc3cb7be43443a383a6a1bf3e01ccb
Rob Sameh <rob.sameh@gmail.com>
2022-05-07 20:38:43 -0700

    added UPDATE word ability

--- app.rb
+++ app.rb
@@ -23,6 +23,18 @@ get('/words/:id') do
   erb(:word)
 end
 
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:name])
+  @words = Word.all
+  erb(:words)
+end
+
 post('/words') do
   name = params[:word_name]
   word = Word.new(name, nil)
