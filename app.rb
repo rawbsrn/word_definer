39541e636b59609b77b1b448d7b735cdcc7770d4
Rob Sameh <rob.sameh@gmail.com>
2022-05-07 23:41:30 -0700

    added basic CRUD functionality for definitions

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -28,11 +29,23 @@ get('/words/:id/edit') do
   erb(:edit_word)
 end
 
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
 patch('/words/:id') do
   @word = Word.find(params[:id].to_i())
   @word.update(params[:name])
   @words = Word.all
   erb(:words)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:name], @word.id)
+  erb(:word)
 end
 
 post('/words') do
@@ -43,6 +56,13 @@ post('/words') do
   erb(:words)
 end
 
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
 delete('/words/:id') do
   @word = Word.find(params[:id].to_i())
   @word.delete()
@@ -50,3 +70,9 @@ delete('/words/:id') do
   erb(:words)
 end
 
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
