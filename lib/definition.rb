1e27f28a61315202948ea353043ff06d10b15855
Rob Sameh <rob.sameh@gmail.com>
2022-05-07 21:15:39 -0700

    added backend definition functionality

---
+++ lib/definition.rb
@@ -0,0 +1,58 @@
+class Definition
+  attr_reader :id
+  attr_accessor :name, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(name, word_id, id)
+    @name = name
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+  def ==(definition_to_compare)
+    (self.name() == definition_to_compare.name()) && (self.word_id() == definition_to_compare.word_id())
+  end
+
+  def self.all
+    @@definitions.values
+  end
+
+  def save
+    @@definitions[self.id] = Definition.new(self.name, self.word_id, self.id)
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
+
+  def update(name, word_id)
+    self.name = name
+    self.word_id = word_id
+    @@definitions[self.id] = Definition.new(self.name, self.word_id, self.id)
+  end
+
+  def delete
+    @@definitions.delete(self.id)
+  end
+
+  def self.clear
+    @@definitions = {}
+  end
+
+  def self.find_by_word(wrd_id)
+    definitions = []
+    @@definitions.values.each do |definition|
+      if definition.word_id == wrd_id
+        definitions.push(definition)
+      end
+    end
+    definitions
+  end
+
+  def word
+    Word.find(self.word_id)
+  end
+
+end
